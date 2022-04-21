e76420fc7bf74895476830fbbea53b15f717bcbb
HeeTae <53458059+XOXOT@users.noreply.github.com>
2022-04-22 00:36:40 +0900

    Update Program.cs

--- C# youtube/3강/3_3사용자지정형/Solution3/ConsoleApplication22/Program.cs
+++ C# youtube/3강/3_3사용자지정형/Solution3/ConsoleApplication22/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication22
         static void Main(string[] args)
         {
             int[] Array1 = { 1, 2, 3 };
-            int[] RefArray;  // 참조형 배열 변수
+            int[] RefArray;  // 참조형 배열 변수 별명이다!
             RefArray = Array1;
             RefArray[1] = 20;
             Console.WriteLine("{0} {1} {2}", Array1[0], Array1[1], Array1[2]);
