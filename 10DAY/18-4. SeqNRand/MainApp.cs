18bc7e6d1579353f9a897906e4c8b1ab52eb85ba
heetae choi <1418083@donga.ac.kr>
2022-05-03 17:08:12 +0900

    C#

---
+++ 10DAY/18-4. SeqNRand/MainApp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace SeqNRand
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Stream outStream = new FileStream("a.dat", FileMode.Create);
+            Console.WriteLine($"Position : {outStream.Position}");
+
+
+            //순차접근
+            outStream.WriteByte(0x01);
+            Console.WriteLine($"Position : {outStream.Position}");
+
+            outStream.WriteByte(0x02);
+            Console.WriteLine($"Position : {outStream.Position}");
+
+            outStream.WriteByte(0x03);
+            Console.WriteLine($"Position : {outStream.Position}");
+
+            //임의접근
+            outStream.Seek(5, SeekOrigin.Current);
+            Console.WriteLine($"Position : {outStream.Position}");
+            
+            //순차접근
+            outStream.WriteByte(0x04);
+            Console.WriteLine($"Position : {outStream.Position}");
+
+            outStream.Close();
+        }
+    }
+}
