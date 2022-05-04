888be1668e31fec7cf76aaf87ad1107d091fbc9d
heetae choi <1418083@donga.ac.kr>
2022-05-04 12:56:23 +0900

    C#

---
+++ 11DAY/19-2. AbortingThread/MainApp.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading;
+
+namespace AbortingThread
+{
+    class SideTask
+    {
+        int count;
+
+        public SideTask(int count)
+        {
+            this.count = count;
+        }
+
+        public void KeepAlive()
+        {
+            try
+            {
+                while (count > 0)
+                {
+                    Console.WriteLine($"{count--} left");
+                    Thread.Sleep(10);
+                }
+                Console.WriteLine("Count : 0");
+            }
+            catch (ThreadAbortException e) //예외 처리
+            {
+                Console.WriteLine(e);
+                Thread.ResetAbort();
+            }
+            finally
+            {
+                Console.WriteLine("Clearing resource...");
+            }
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            SideTask task = new SideTask(100);
+            Thread t1 = new Thread(new ThreadStart(task.KeepAlive));
+            t1.IsBackground = false;
+
+            Console.WriteLine("Starting thread...");
+            t1.Start();
+
+            Thread.Sleep(100);
+
+            Console.WriteLine("Aborting thread...");
+            //t1.Abort(); //스레드 취소(catch 절 실행) -- net 5.0에서는 누락시키는 것이 좋다 오류가 뜸 
+            t1.Interrupt(); //대신 인터럽트를 써서 오류 해결
+
+            Console.WriteLine("Wating until thread stops...");
+            t1.Join();
+            
+            Console.WriteLine("Finished");
+        }
+    }
+}
